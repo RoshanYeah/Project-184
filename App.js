2bdcc83e35d1e2f5fb594ceb91ab00dce6198d3e
RoshanYeah <98729871+RoshanYeah@users.noreply.github.com>
2023-05-04 07:59:08 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './screens/Main.js'
+
+export default function App(){
+  return(
+   <Main/>
+  )
+}
